a109df388543a11a5d863b45d75b3632077f1a8c
luoh17 <luoh17@vanke.com>
2018-08-02 11:12:44 +0800

    加入对A价款总价的前置判断测试

--- newapp/src/main/java/com/lehow/newapp/MainActivity.java
+++ newapp/src/main/java/com/lehow/newapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.widget.Toast;
 import com.lehow.newapp.base.FlexEntity;
 import com.lehow.newapp.base.FlexField;
 import com.lehow.newapp.base.FieldProxyAdapter;
@@ -32,7 +34,27 @@ public class MainActivity extends AppCompatActivity {
     recyclerView.setItemAnimator(null);//这里要取消动画，否则在notifyItemChanged时，会onCreateViewHolder 生成一个新的ViewHolder来做动画的过度，
     // 如果当前的item中含有EditText，会导致我在Holder中记录的光标位置无效。当然也可以考虑在别的地方记录光标位置，目前放在这
 
+    flexEntity.setFieldClickListener("aamount", new FlexFieldProcessor() {
+      @Override public void onFieldClick(Activity activity, FlexField flexField) {
 
+      }
+
+      @Override public void onChange(FlexField flexField, Bundle bundle) {
+        if (bundle != null) {//级联通知的更新
+          String curInput = bundle.getString("value");
+          float newValue = TextUtils.isEmpty(curInput) ? 0f : Float.parseFloat(curInput);
+          if (newValue > 10000) {
+            Toast.makeText(MainActivity.this, "A价款总价 不能超过总房款价", Toast.LENGTH_SHORT).show();
+            flexField.setValue(
+                flexField.getValue());//通知刷新当前的输入，比如前面一次有效输入是10000，现在输入是10100，这个是将输入框的显示还原为10000
+            flexField.setSummary(String.valueOf(flexField.getValue()));
+          } else {
+            flexField.setValue(newValue);
+            flexField.setSummary(curInput);
+          }
+        }
+      }
+    });
 /*
  *观察值，做自更新
  flexEntity.setFieldClickListener("aLoanType", new SimpleSelectFieldProcessor(R.array.loan_type,R.array.loan_type,1){
